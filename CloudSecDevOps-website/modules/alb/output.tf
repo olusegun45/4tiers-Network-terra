d5a7a5504e8f6ffaf66bf9d95cb39521d6ecf5ed
pogunyemi45 <pogunyemi45@gmail.com>
2023-01-15 15:27:47 -0600

    cloudsec

---
+++ CloudSecDevOps-website/modules/alb/output.tf
@@ -0,0 +1,23 @@
+output "frontend-alb_target_group_arn" {
+  value = aws_lb_target_group.frontend-alb_target_group.id
+}
+
+output "frontend-alb_dns_name" {
+  value = aws_lb.Prod-Frontend-LB.dns_name
+}
+
+output "frontend-alb_zone_id" {
+  value = aws_lb.Prod-Frontend-LB.zone_id
+}
+
+output "Backend-alb_target_group_arn" {
+  value = aws_lb_target_group.Backend-alb_target_group.id
+}
+
+output "Backend-alb_dns_name" {
+  value = aws_lb.Prod-Backenfend-LB.dns_name
+}
+
+output "Backend-alb_zone_id" {
+  value = aws_lb.Prod-Backenfend-LB.zone_id
+}
